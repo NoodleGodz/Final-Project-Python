86ce1ae1def1c1ae341f08f8334c00352a79a3d8
tle <thongld.bi11-257@st.usth.edu.vn>
2022-04-30 13:16:55 +0700

    Update: password encryption; pickle to object file; args function

--- user_info/User.py
+++ user_info/User.py
@@ -1,9 +1,29 @@
 import hashlib
-from msvcrt import getch
 import os
 import getpass
 import sys
 
+# NOT DONE:
+# - grabpass function
+# - Delete user function
+
+## Check file 
+def check(file, string):
+    with open(file, 'r') as read:
+        for line in read:
+            if string in line:
+                return True
+    return False
+
+def checklogin(file, string):
+    global lf
+    with open(file, 'r') as read:
+        for (lf, line) in enumerate(read):
+            if string in line:
+                return True
+    return False
+
+## Get password from user input - use in later function
 def mpass(prompt='Password: '):
     if sys.stdin is not sys.__stdin__:
         pwd = getpass.getpass(prompt)
@@ -28,74 +48,102 @@ def mpass(prompt='Password: '):
                 sys.stdout.write('*')
                 sys.stdout.flush()
                 pwd = pwd + char
+        return pwd
 
-def check(file, string):
-    with open(file, 'r') as read:
-        for line in read:
-            if string in line:
-                return True
-    return False
-
-def grabpass():
-    global ptc
+## Read pass
+def grabpass(password):
+    global pass_to_check
     writef = open("user_database.obj", "r")
     cl = lf + 1
     ltr = [cl]
     for position, line in enumerate(writef):
         if position in ltr:
-            ptc = line
-
-def checklogin(file, string):
-    global lf
-    with open(file, 'r') as read:
-        for (lf, line) in enumerate(read):
-            if string in line:
-                return True
-    return False
-
-def clear():
-    os.system("cls")
-
-def add_user():
+            pass_to_check = line    
+    
+## Add user (Sign up)
+# IMPORTANT: must have the password input from mpass() function
+# username = input("Username: ")
+# password = mpass()
+# password is encrypted with sha256, i.e. can't be decrypted
+def add_user(username, password):
     while True:
-        a = input("Username: ")
-        h = hashlib.pbkdf2_hmac('sha256', a.encode("utf-8"), b'(*!@#s', 823)
-        newuser = h.hex()
+        newuser = username
         if check('user_database.obj', newuser):
             print("User already exists!")
         else:
             break
-    b = mpass()
-    h = hashlib.pbkdf2_hmac('sha256', b.encode("utf-8"), b'*@#d2', 182)
-    newpass = h.hex()
-    users = open("user_database.obj", "a")
-    users.write("Username: " + newuser + "\nPassword: " + newpass + "\n\n")
-    users.close()
-    clear()
-    print("The following has been saved to the user database...\n")
-    print("Username: " + newuser + "\nPassword: " + newpass + "\n")
-    print("Press any key to continue...")
-    getch()
-    clear()
+    h = hashlib.pbkdf2_hmac('sha256', password.encode("utf-8"), b'*@#d2', 182)
+    newpass = h.hex() 
+    # Add item to list
+    users = { 
+        "user_name": newuser,
+        "passwd": newpass
+    }
+    user_list = []
+    user_list.add(users)
+    # open(file, mode=""), for a is writing to the db
+    with open("user_database.obj", "a") as handle:
+        pickle.dumps(user_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    # remember to have a successfully message!
 
+## Get user from db
 def read_db():
+    ## IMPORTANT - Check if user_db is empty or not
     if os.path.getsize("user_database.obj") == 0:
-        clear()
-        print("Database is empty!\n")
+        #print("Database is empty!\n")
+        pass
     else:
-        clear()
-        print("User Database:\n")
-        reader = open("user_database.obj", "r")
-        print(reader.read())
-        reader.close()
-        print("Press any key to continue...")
-        getch()
-        clear()
+        with open("user_database.obj", "rb") as handle:
+            user_db = pickle.loads(handle)
+            for i in user_db:
+                for k in user_db[i]:
+                    username = next(iter(user_db[i]))
+        # return username
 
+## Log in
+# IMPORTANT: must have the password input from mpass() function
+# username = input("Username: ")
+# password = mpass()
+def log_in(username, password):
+    if os.path.getsize("user_database.obj") == 0:
+        #print("Database is empty!\n")
+        pass
+    else:
+        while True:
+            userinput = username
+            with open("user_database.obj", "rb") as handle:
+                user_db = pickle.loads(handle)
+                if checklogin(userinput in user_db.value()):
+                    print("Welcome " + username + "!\n")
+                    break
+                else:
+                    print("Invalid username.")
+        while True:
+            h = hashlib.pbkdf2_hmac('sha256', password.encode("utf-8"), b'*@#d2', 182)
+            passinput = h.hex()
+            grabpass()
+            ptct = pass_to_check.replace("Password: ", '')
+            ptcr = pass_to_check.rstrip("\n")
+            if passinput == ptcr:
+                currentuser = userinput
+                clear()
+                print("Successfully logged in as " + currentuser + "!\n")
+                usrtitle = ("title Secure Login System (logged in as " + currentuser + ")")
+                os.system(usrtitle)
+                break
+            else:
+                print("Invalid Password.")
+
+def delete_user():
+    pass
+
+## Wipe all db
+# For this part, it is better to integrated it with the gui
+# idk why I want to delete all data, lol!
 def wipe_db():
     if os.path.getsize("user_database.obj") == 0:
-        clear()
-        print("Database is already empty!\n")
+        #print("Database is already empty!\n")
+        pass
     else:
         while True:
             a = input("Are you sure you want to wipe the user database [y/n]? ")
@@ -112,43 +160,3 @@ def wipe_db():
                 break
             else:
                 print("Invalid selection.")
-
-def log_in():
-    if os.path.getsize("user_database.obj") == 0:
-        clear()
-        print("Database is empty!\n")
-    else:
-        while True:
-            a = input("Username: ")
-            h = hashlib.pbkdf2_hmac('sha256', a.encode("utf-8"), b'(*!@#s', 823)
-            userinput = h.hex()
-            if checklogin('user_database.obj', userinput):
-                print("Welcome " + a + "!\n")
-                break
-            else:
-                print("Invalid username.")
-        while True:
-            b = mpass()
-            h = hashlib.pbkdf2_hmac('sha256', b.encode("utf-8"), b'*@#d2', 182)
-            passinput = h.hex()
-            grabpass()
-            ptct = ptc.replace("Password: ", '')
-            ptcr = ptct.rstrip("\n")
-            if passinput == ptcr:
-                currentuser = a
-                clear()
-                print("Successfully logged in as " + currentuser + "!\n")
-                usrtitle = ("title Secure Login System (logged in as " + currentuser + ")")
-                os.system(usrtitle)
-                break
-            else:
-                print("Invalid Password.")
-                
-def delete_user():
-    pass
-            
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
