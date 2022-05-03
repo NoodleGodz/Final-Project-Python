9f126a2b2f69a6ded837189b2eae87d33d7d171b
tle <thongld.bi11-257@st.usth.edu.vn>
2022-05-03 18:30:32 +0700

    Update: module for user - debugged

--- Elec_Database/User_List.py
+++ Elec_Database/User_List.py
@@ -1,13 +1,28 @@
 import Elec_Database.User_module as User_module
 import pickle
+import os
+import getpass
+import sys
 
-class User:
+if os.name == "nt":
+    from msvcrt import getch
+else:
+    from readchar import readchar as getch
+
+is_windows = os.name == "nt"
+is_linux = os.name == "posix"
+
+BACKSPACE_WINDOWS = 8
+BACKSPACE_LINUX = 127
+RETURN_KEY = 13
+
+class User_List:
     """
     This class contains a list of Administrator user
     
     Main function get display on the GUI:
         + log_in(username,password)
-        + register(username,password)
+        + user_register(username,password)
         + read_userdb()
         + delete_user(username)
     
@@ -17,53 +32,134 @@ class User:
         + A button to delete specific username in the main panel after display all username from above.
         
     Note:
-        + Registration password must be used with the specific function from User_module.
+        + Registration password must be used with the specific input_passwd function.
     """
     def __init__(self, username, password) -> None:
         self.username = username
         self.password = password
         
+    ## Register (Sign up)
     def register(self, username, password):
-        User_module.register(self.username, self.password)
-        return
-    
-    def log_in(self, username, password):
-        User_module.log_in(self.username, self.password)
-
-    def read_userdb(self):
         """
-        This function to list all user.
-        
+        This function to sign up then save to pickle files.  
+        Parameter:
+            username
+            password  
         Return:
-            list of username
+            Save username and password to object file
+        """
+        while True:
+            if User_module.check_empty_user_db():
+                break
+            else:
+                if not User_module.check_if_exist(self.username):
+                    # Continue with the input
+                    break
+                else:
+                    # username exist!
+                    return False
+        newuser = self.username
+        newpass = User_module.password_encryption(self.password)
+        # Add item to list of dictionary
+        user_dict = { 
+            "user_name": newuser,
+            "passwd": newpass
+        }
+        user_db = User_module.load_pickle("Object_Folder/user_database.obj")
+        user_db.append(user_dict)
+        User_module.write_pickle("Object_Folder/user_database.obj", user_db)
+
+    def input_passwd(prompt='Password: '):
+        """
+        This function to get a password from the user and display a masked value at the prompt. It made to be used with the later function like encrypting the user input password.
+        Parameter:
+            (user input password)
+        Return:
+            masked password at the prompt (e.g.: 123456 -> ******)
+        """
+        if sys.stdin is not sys.__stdin__:
+            pwd = getpass.getpass(prompt)
+            return pwd
+        else:
+            pwd = ""
+            sys.stdout.write(prompt)
+            sys.stdout.flush()
+            while True:
+                key = ord(getch())
+                if key == RETURN_KEY:
+                    sys.stdout.write("\n")
+                    return pwd
+                    break
+                if (
+                    is_windows
+                    and key == BACKSPACE_WINDOWS
+                    or is_linux
+                    and key == BACKSPACE_LINUX
+                ):
+                    if len(pwd) > 0:
+                        # Erases previous character.
+                        sys.stdout.write("\b" + " " + "\b")
+                        sys.stdout.flush()
+                        pwd = pwd[:-1]
+                else:
+                    # Masks user input.
+                    char = chr(key)
+                    sys.stdout.write("*")
+                    sys.stdout.flush()
+                    pwd = pwd + char
+        
+    ## Log in
+    def user_log_in(self, username, password):
+        """
+        This function to check the log in from user.
+        Paramter:
+            username
+            password
+        Return:
+            True: successfullry login
+            False: invalid login
         """
         if User_module.check_empty_user_db():
-            #Empty database
-            break
+            return
         else:
-            read_db = []
-            with open("Object_Folder/user_database.obj", "rb") as handle:
-                user_db = pickle.loads(handle)
-                for i in range(len(user_db)):
-                    read_db.append(next(iter(user_db[i])))
-            return read_db
-    
+            while True:
+                userinput = self.username
+                if User_module.check_if_exist(userinput):
+                    break
+                else:
+                    continue
+            while True:
+                passinput = User_module.password_encryption(password)
+                passcheck = User_module.check_passwd_from_db(userinput)
+                if passinput == passcheck:
+                    return True
+                else:
+                    return False
+
+    def read_db(self):
+        """
+        This function calls the load_user_db function from User_module
+        """
+        User_module.load_user_db()
+                
     def delete_user(self, username):
         """
         This function to delete one user.
-        Data is deleted from user database object
-        
+        Data is deleted from user database object.
         Return:
             void
         """
         if User_module.check_empty_user_db():
             #Empty database
-            break
+            return
         else:
-            with open("Object_Folder/user_database.obj", "w+b") as handle:
-                user_db = pickle.loads(handle)
-                if check_if_exist(username):
-                    for i in range(len(user_db)):
-                        if user_db[i]['user_name'] = username:
-                            del user_db[i]
-                    pickle.dumps(user_db, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            user_db = User_module.load_pickle("Object_Folder/user_database.obj")
+            print(user_db)
+            if User_module.check_if_exist(username):
+                for i in range(len(user_db)):
+                    if next(iter(user_db[i].items()))[1] == username:
+                        del user_db[i]
+                User_module.write_pickle("Object_Folder/user_database.obj", user_db)
+            else:
+                #no user to delete
+                return
--- Elec_Database/User_module.py
+++ Elec_Database/User_module.py
@@ -2,184 +2,100 @@ import hashlib
 import os
 import getpass
 import sys
+import pickle
+from itertools import islice
 
-def input_username():
-    pass
-
-if os.name == "nt":
-    from msvcrt import getch
-else:
-    from readchar import readchar as getch
-
-is_windows = os.name == "nt"
-is_linux = os.name == "posix"
-
-BACKSPACE_WINDOWS = 8
-BACKSPACE_LINUX = 127
-RETURN_KEY = 13
-
-def input_passwd(prompt='Password: '):
+def load_pickle(filename):
     """
-    This function to get a password from the user and display a masked value at the prompt. It made to be used with the later function like encrypting the user input password.
-    
+    This function to file from file   
     Parameter:
-        (user input password)
-        
+        filename
     Return:
-        masked password at the prompt (e.g.: 123456 -> ******)
+        list of item from saving files
     """
-    if sys.stdin is not sys.__stdin__:
-        pwd = getpass.getpass(prompt)
-        return pwd
-    else:
-        pwd = ""
-        sys.stdout.write(prompt)
-        sys.stdout.flush()
+    objects = []
+    with open(filename, "rb") as handle:
         while True:
-            key = ord(getch())
-            if key == RETURN_KEY:
-                sys.stdout.write("\n")
-                return pwd
+            try:
+                objects.extend(pickle.load(handle))
+            except EOFError:
                 break
-            if (
-                is_windows
-                and key == BACKSPACE_WINDOWS
-                or is_linux
-                and key == BACKSPACE_LINUX
-            ):
-                if len(pwd) > 0:
-                    # Erases previous character.
-                    sys.stdout.write("\b" + " " + "\b")
-                    sys.stdout.flush()
-                    pwd = pwd[:-1]
-            else:
-                # Masks user input.
-                char = chr(key)
-                sys.stdout.write("*")
-                sys.stdout.flush()
-                pwd = pwd + char
-
-## Check user name exist
-def check_if_exist(username):
+    return objects
+                
+def write_pickle(filename, object):
     """
-    This function to check if the username exist in the object database or not.
-    
-    Parameter: 
-        username
-    
+    This function to pickle to file
+    Parameter:
+        filename
     Return:
-        True: username exist
-        False: username not exist
+        void
     """
-    read_data = []
-    with open("Object_Folder/user_database.obj", "rb") as handle:
-        user_db = pickle.loads(handle)
-        pickle.loads(handle)
-        for i in range(len(user_db)):
-            read_data.append(next(iter(user_db[i])))
-        if username in read_data:
-            return True
-        else:
-            return False
-
+    with open(filename, "wb") as handle:
+        pickle.dump(object, handle)      
+                
 ## Check password exist
 def check_passwd_from_db(username):
     """
     This function to get the password from the database, for using it for double check later.
-    
     Parameter:
         username
-        
     Return:
         user password with the following username
     """
-    with open("Object_Folder/user_database.obj", "rb") as handle:
-        user_db = pickle.loads(handle)
-        for i in range(len(user_db)):
-            val = user_db[i].values()
-            user, passwd = next(val), next(val)
-            if username = user:
-                password = passwd
-                break
-        return password
-        
-## Register (Sign up)
-def register(username, password):
+    user_db = load_pickle("Object_Folder/user_database.obj")
+    for i in range(len(user_db)):
+        user, passwd = islice(user_db[i].values(), 2)
+        if username == user:
+            password = passwd
+    return password
+
+def load_user_db():
     """
-    This function to sign up then save to pickle files.
-    
-    Parameter:
-        username
-        password
-        
+    This function to get the username from the list of database
     Return:
-        Save username and password to object file
+        List of username
     """
-    while True:
-        if not check_if_exist(username):
-            # Continue with the input
-            break
-        else:
-            # username exist!
-            return False
-            
-    h = hashlib.pbkdf2_hmac('sha256', password.encode("utf-8"), b'*@#d2', 182)
-    newpass = h.hex() 
-    
-    # Add item to list of dictionary
-    users = { 
-        "user_name": newuser,
-        "passwd": newpass
-    }
-    user_list = []
-    user_list.append(users)
-    
-    # Write file to database
-    with open("Object_Folder/user_database.obj", "w+b") as handle:
-        user_db = pickle.loads(handle)
-        user_db.extend(user_list)
-        pickle.dumps(user_db, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    read_data = []
+    user_db = load_pickle("Object_Folder/user_database.obj")
+    for i in range(len(user_db)):
+        read_data.append(next(iter(user_db[i].items()))[1])
+    return read_data
+
+def check_if_exist(username):
+    """
+    This function to check if the username exist in the object database or not.
+    Parameter: 
+        username
+    Return:
+        True: username exist
+        False: username not exist
+    """
+    user_lists = load_user_db()
+    if username in user_lists:
+        return True
+    else:
+        return False
 
 def check_empty_user_db():
     """
     This function to check if the user database is empty or not
-    
     Return:
         True if empty
         False if not empty
     """
     if os.path.getsize("Object_Folder/user_database.obj") == 0:
         return True
-    return False
-    
-## Log in
-def log_in(username, password):
-    """
-    This function to check the log in from user.
-    
-    Paramter:
-        username
-        password
-    
-    Return:
-        True: successfullry login
-        False: invalid login
-    """
-    if check_empty_user_db():
-        break
     else:
-        while True:
-            userinput = username
-            with open("Object_Folder/user_database.obj", "rb") as handle:
-                if check_exist("Object_Folder/user_database.obj", userinput):
-                    break
-                else:
-                    continue
-        while True:
-            h = hashlib.pbkdf2_hmac('sha256', password.encode("utf-8"), b'*@#d2', 182)
-            passinput = h.hex()
-            passcheck = check_passwd_from_db(userinput)
-            if passinput == passcheck:
-                return True
-            else:
-                return False
+        return False
+    
+def password_encryption(password):
+    """
+    This function to encrypt the password to SHA256 - nondecrypt password
+    Parameter:
+        password
+    Return:
+        Encrypted pass
+    """
+    h = hashlib.pbkdf2_hmac('sha256', password.encode("utf-8"), b'*@#d2', 182)
+    return h.hex()
+    
